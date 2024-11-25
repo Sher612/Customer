587ae5b21105432205a9d2340724f7bfc06d3120
Sher Afgan Tareen <sher@refugeeassistancealliance.org>
2024-11-25 15:29:46 -0500

    Checkin commit

--- src/Customer.java
+++ src/Customer.java
@@ -2,20 +2,20 @@ import java.util.Scanner;
 
 public class Customer {
     String[] customers = new String[5];
+    String str1;
+    String str2;
     Scanner input = new Scanner(System.in);
     public void inputCustomerData() {
-        String str1;
-        String str2;
         for(int i=0; i< customers.length; i++) {
             do {
                 System.out.print("Enter customer first name: ");
                 str1= input.nextLine();
                 System.out.print("Enter customer last name: ");
                 str2= input.nextLine();
-                if(str1.isEmpty()||str2.isEmpty())
+                if(str1.trim().isEmpty()||str2.trim().isEmpty())
                 System.out.println("First name and last name can not be empty");
             }
-            while(str1.isEmpty()||str2.isEmpty());
+            while(str1.trim().isEmpty()||str2.trim().isEmpty());
             customers[i]= str1 + " " + str2;
         }
     }
